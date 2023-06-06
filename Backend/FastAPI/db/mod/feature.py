15d1b0f1710691c8a9fe5e771faf082408266a74
DellacquaD <d.dellacquaruiz@gmail.com>
2023-06-06 15:11:19 -0300

    elimino carpeta de modulos

--- Backend/FastAPI/db/mod/feature.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from .base import Base
-
-class Feature(Base):
-    __tablename__ = 'feature'
-
-    feature_id = Column(Integer, primary_key=True)
-    name = Column(String(255))
